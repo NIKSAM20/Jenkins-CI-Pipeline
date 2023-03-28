56397f0feb9cf4a84e1b8da2dd46d5923e8325e8
NIKSAM20 <nikhilbisht0026@gmail.com>
2023-03-28 10:34:01 +0530

    first commit

---
+++ First.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void power(int a, int b){
+    int p=1;
+    for(int i=0; i<b; i++){
+        p=p*a;
+    }
+
+    cout << a <<" raise to " <<b <<" =" <<p <<endl;
+}
+
+int main(){
+
+    int x, y;
+    cout <<"Enter two numbers: ";
+    cin >>x >>y;
+    
+    power(x, y);
+}
